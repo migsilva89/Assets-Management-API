338bdd9c7c4b59a2d3fcf3b10b549f1e2334f5d2
migsilva89 <migdrum@gmail.com>
2023-03-14 01:55:16 +0000

    register route, modal, controller.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,18 @@
-const registerUser = (req, res) => {
-  res.send('ok')
+const User = require('../models/User')
+
+const registerUser = async (req, res) => {
+  const { name, email, password, nickName } = req.body
+  console.log(name, email, password, nickName)
+  
+  try {
+    const user = await User.create({ name, email, password, nickName })
+    return res.send({ user })
+  } catch (err) {
+    return res.status(400).send({ error: 'Registration failed!' })
+  }
 }
 
-const loginUser = (req, res) => {
+const loginUser = async (req, res) => {
   res.send('ok')
 }
 
